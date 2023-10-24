649a3b224631ce52275ffab47f7b0f60c4667622
Serhii Herych <mac@MacBook-Air-mac.local>
2023-10-24 23:21:44 +0300

    rename file actions - handler; creact Actiont add new methods(innderHTML, eq, index, find)

--- src/js/lib/lib.js
+++ src/js/lib/lib.js
@@ -1,7 +1,8 @@
 import $ from "./core.js";
 import './modules/display.js';
 import './modules/classes.js';
-import './modules/actions.js'
+import './modules/handlers.js'
 import './modules/attribute.js'
+import './modules/actions.js'
 
 export default $;
